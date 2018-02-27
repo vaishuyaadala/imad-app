3c20aff9aaefbc83f0a71150ac1d6539cec668e9
vaishuyaadala <36795607+vaishuyaadala@users.noreply.github.com>
2018-02-27 22:25:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,9 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='New value';
+var button=document.getElementById('counter');
+var counter=0;
 
-var img=document.getElementById('madi');
-marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft+'px';
-}
-
-
-img.onclick=function() {
-    var interval=setInterval(moveRight,50);
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
 };
