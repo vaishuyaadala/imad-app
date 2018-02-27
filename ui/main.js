1153a984b705a6ecbdc142a5b371b5707b65b5d5
vaishuyaadala <36795607+vaishuyaadala@users.noreply.github.com>
2018-02-27 21:05:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+
+var img=document.getElementById('img');
+img.onclick=function() {
+    img.style.marginLeft='100px';
+};
