ebd7514b6c70c7301b68971bb4e671d2ae9ff5ff
vaishuyaadala <36795607+vaishuyaadala@users.noreply.github.com>
2018-02-27 22:12:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,12 @@ app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
 
 
 
