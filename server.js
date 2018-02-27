93d3e515fc81990c66df8cd9070cb0623529f9ad
vaishuyaadala <36795607+vaishuyaadala@users.noreply.github.com>
2018-02-27 20:07:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-   res.send(createTemplate(Article-one));
+   res.send(createTemplate(ArticleOone));
 });
 app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-two.html'));
