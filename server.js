f9f1b5f039adbe1cf2678d1b13c8ab29b4a0fe1a
vaishuyaadala <36795607+vaishuyaadala@users.noreply.github.com>
2018-03-06 21:27:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,15 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
+
+var names=[];
+app.get('/submit-name',function(req,res){
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+    
+});
+
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
@@ -106,13 +115,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-var names=[];
-app.get('/submit-name',function(req,res){
-    var name=req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
 });
 
 
