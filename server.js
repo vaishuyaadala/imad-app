2e3445119f2e5b013817b8f2a1c53b04aab8bcb3
vaishuyaadala <36795607+vaishuyaadala@users.noreply.github.com>
2018-04-01 22:38:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,125 +5,35 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles={
-"article-one" :{
-    title:'Article one |vaishnavi yadala',
-    heading:'Article one',
-    date:'Sep-10-1999',
-    content:`            <p>The kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first article
-                     </p>
-                     <p>The kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first article 
-</p>`
-      
-},
-"article-two":{
-    title:'Article two |vaishnavi yadala',
-    heading:'Article two',
-    date:'Sep-10-1999',
-    content:`            <p>The kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first article
-                     </p>
-                     <p>The kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first articleThe kis the content for my first article 
-</p>`
-      
-                   
-},
-};
-function createTemplate (data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-    
-
-  var htmlTemplate=`
-<html>
-<head>
-    <title> ${title}</title>
-     <link href="/ui/style.css" rel="stylesheet" />
-   
-</head>    
-
-
-    <body>
-         <div class="container">
-            <div>
-            <a href="/">Home</a>
-             
-           </div >
-             <hr/>
-                 <h3>${heading}</h3>
-           <div>
-            ${date}
-           </div>
-               <div>
-               ${content}
-          </div>
-        
-    </body>
-    
-    
-</html>
-`;
-return htmlTemplate;
-
-}
 
 
 
 
 
 
-app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+
+
+
+
+$(function() {
+
+        if (localStorage["name"]) {
+            $('#name').val(localStorage["name"]);
+        }
+        if (localStorage["email"]) {
+            $('#email').val(localStorage["email"]);
+        }
+		if (localStorage["message"]) {
+            $('#message').val(localStorage["message"]);
+        }
+
 });
 
-
-var counter=0;
-app.get('/counter',function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
+$('.stored').change(function () {
+    localStorage[$(this).attr('name')] = $(this).val();
 });
-
-
-var names=[];
-app.get('/submit-name',function(req,res){
-    var name=req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
-});
-
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
-});
-
-
-
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+	
+$('#contactForm').submit(function() {
+	localStorage.clear();
+  $('.stored').val("");
 });
